468cc320f9c497f1dd73ec564cc51965505b7936
Rajkumar17 <rajkumarm1703@gmail.com>
2024-12-04 18:37:01 +0530

    data types

---
+++ task_2.py
@@ -0,0 +1,31 @@
+# arithmetic operators(+ - * / % ** // )
+a = 5
+b = 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
+
+# relational operators (+= -= *= /= //= %= )
+a+=2
+print(a)
+a-=2
+print(a)
+a*=3
+print(a)
+a/=2
+print(a)
+
+
+# logical operators (AND OR NOT)
+a=5
+b=6
+c=4
+print(a>a & a>c)
+print(a>b or b>c)
+print(not(a>b))
+
+
